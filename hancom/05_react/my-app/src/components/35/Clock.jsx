49d6df4df0332de57078462e33410964d125a09d
hugo <vvhooping@gmail.com>
2026-07-10 16:00:52 +0900

    feat: 05_react 35번 Clock 컴포넌트 실습 추가
    setInterval과 cleanup으로 실시간 시계를 표시하는 실습.

---
+++ hancom/05_react/my-app/src/components/35/Clock.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export default function Clock() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return <p>⏰ {time}</p>;
+}
